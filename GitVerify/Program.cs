9e8da957e910bbf7ad971c198ac555c57028fc38
gg <ghetigradin@gmail.com>
2019-05-10 22:45:34 +0300

    Add project files.

---
+++ GitVerify/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitVerify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
