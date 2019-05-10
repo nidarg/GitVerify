c1886be1166de8d68197134ae334c251426ced8b
gg <ghetigradin@gmail.com>
2019-05-10 23:13:32 +0300

    Test Add function

--- GitVerify/Program.cs
+++ GitVerify/Program.cs
@@ -2,11 +2,15 @@
 
 namespace GitVerify
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        public static int  Add(int x,int y)
+        {
+            return x+y;
         }
     }
 }
--- XUnitGit/UnitTest1.cs
+++ XUnitGit/UnitTest1.cs
@@ -5,10 +5,12 @@ namespace XUnitGit
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(3,5,8)]
+        public void TestAdd(int x,int y,int expected)
         {
-
+            int actual = x+y;
+            Assert.Equal(expected,actual);
         }
     }
 }
